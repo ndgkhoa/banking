91aca4e7f73b6569a6e152e7fbe9fb928f44ec1a
khoa <user.iamjasper@gmail.com>
2024-10-01 00:24:01 +0700

    config plaid

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -13,6 +13,7 @@ import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { signIn, signUp } from '@/lib/actions/user.actions'
+import PlaidLink from './PlaidLink'
 
 const AuthForm = ({ type }: { type: string }) => {
     const router = useRouter()
@@ -28,13 +29,25 @@ const AuthForm = ({ type }: { type: string }) => {
             password: '',
         },
     })
-
+    const a = 1
     const onSubmit = async (data: z.infer<typeof formSchema>) => {
         setIsLoading(true)
 
         try {
             if (type === 'sign-up') {
-                const newUser = await signUp(data)
+                const userData = {
+                    firstName: data.firstName!,
+                    lastName: data.lastName!,
+                    address1: data.address1!,
+                    city: data.city!,
+                    state: data.state!,
+                    postalCode: data.postalCode!,
+                    dateOfBirth: data.dateOfBirth!,
+                    ssn: data.ssn!,
+                    email: data.email,
+                    password: data.password,
+                }
+                const newUser = await signUp(userData)
                 setUser(newUser)
             }
             if (type === 'sign-in') {
@@ -84,8 +97,10 @@ const AuthForm = ({ type }: { type: string }) => {
                     </h1>
                 </div>
             </header>
-            {user ? (
-                <div className="flex flex-col gap-4">{}</div>
+            {a ? (
+                <div className="flex flex-col gap-4">
+                    <PlaidLink user={user} variant="primary" />
+                </div>
             ) : (
                 <>
                     <Form {...form}>
